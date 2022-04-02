fce0954526294daa9eb0f3b7852a61e62770f3f8
Felipe B <20200754@esen.edu.sv>
2022-04-02 10:48:05 -0600

    Init files

---
+++ config.py
@@ -0,0 +1,11 @@
+class BaseConfig(object):
+    # sqlite:////tmp/test.db
+    """SQLALCHEMY_DATABASE_URI = "mysql://root:root@localhost:3306/testdb" """
+    SQLALCHEMY_DATABASE_URI = "sqlite:///todolist_database.db"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    """
+    SQLALCHEMY_POOL_SIZE = 20
+    SQLALCHEMY_POOL_TIMEOUT = 300
+    """
+    SECRET_KEY = "horrible_secret_key"
+    LOGIN_DISABLED = True
